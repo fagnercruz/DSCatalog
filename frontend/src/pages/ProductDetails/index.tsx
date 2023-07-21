ee644072ac947913b3b3177ed17e9158180f135e
Fagner Cruz <fagner@sti.ufpb.br>
2023-07-21 12:37:11 -0300

    REACT - Implementando o html do productDetails

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 
 import './styles.css';
+import ProductPrice from 'components/ProductPrice';
 
 
 const ProductDetails = () => {
-
 
   return (
     <div className="product-details-container">
@@ -17,26 +17,31 @@ const ProductDetails = () => {
             <h2>VOLTAR</h2>
           </div>
         </Link>
+        
         <div className="row">
+          
           <div className="col-xl-6">
-           
-              <>
+                      
                 <div className="img-container">
-                  <img src="" alt="" />
+                  <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg" alt="alt" />
                 </div>
+                
                 <div className="name-price-container">
-                  <h1>P</h1>
-                  
+                  <h1>Nome do produto</h1>
+                  <ProductPrice valor={12345.69}  />
                 </div>
-              </>
-           
+
           </div>
+          
           <div className="col-xl-6">
+              
               <div className="description-container">
                 <h2>Descrição do produto</h2>
-                <p>p</p>
+                <p>texto aleatorio para descrever o produto</p>
               </div>
+
           </div>
+
         </div>
       </div>
     </div>
